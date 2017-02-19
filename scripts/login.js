87caabd7525f26a490cd799c9d36cfb7e5f9dacb
dlstess <zahary1981@gmail.com>
2017-02-19 21:15:59 +0200

    Last commit

--- scripts/login.js
+++ scripts/login.js
@@ -1,6 +1,5 @@
-
-function onClick()
-{
-    alert('hello');
-
+function loginClick() {
+    let user=document.getElementById('text').value;
+    let password=document.getElementById('password').value;
+    console.log(user);
 }
