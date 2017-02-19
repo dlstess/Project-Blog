f49eefe97ceaf4dd3b8c13863801d3bede1f5a3a
dlstess <zahary1981@gmail.com>
2017-02-20 00:59:09 +0200

    2

---
+++ views/login.js
@@ -0,0 +1,7 @@
+function loginClick() {
+    let user=document.getElementById('text').value;
+    let password=document.getElementById('password').value;
+    console.log(user);
+    console.log('Zaho');
+}
+console.log('hi')
